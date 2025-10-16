d4f05a630ec18cbbdfbd4ca05ba4cb8bcb5c73c4
worthless01 <worthless0302@gmail.com>
2025-10-16 16:27:09 +0000

    qqqqqqqqqq

---
+++ lec_bool.py
@@ -0,0 +1,13 @@
+print(bool (2))
+
+print(bool('Good'))
+
+print(bool([1, 2, 3]))
+
+print(bool (0))
+
+print(bool (''))
+
+print(bool ([]))
+
+print(bool ([[]]))
---
+++ lec_konst_if.py
@@ -0,0 +1,15 @@
+if 1:
+    print('hello 1')
+
+a = 3
+if a > 1:
+   print(f'hello {a}')
+
+b=5
+if b==5: #сравнение
+    print(f'hello {b}')
+
+
+
+
+
