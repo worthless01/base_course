cd2ffddf46ec96a6a31515fe1839f7e2dd5b0b59
worthless01 <worthless0302@gmail.com>
2025-11-06 15:48:08 +0000

    kjhnjkl

--- lec_obj_ref.py
+++
@@ -1,7 +0,0 @@
-s = 3 + 4
-
-memory_id = id(s)
-print(s, memory_id)
-
-s = 5
-print(s)
--- lec_task_6.py
+++
@@ -1,9 +0,0 @@
-a = input('возраст пользователя')
-print(input('пол пользователя'))
-print(input('имя пользователя'))
-print(input('город пользователя'))
-print(input('в каком классе пользователь учится'))
-
-itog = []
-itog.append(a)
-print(itog) 
