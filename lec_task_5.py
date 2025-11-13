c5be10a4a4a674dbd5a87542d6f721f4cc0004ce
worthless01 <worthless0302@gmail.com>
2025-11-13 16:49:17 +0000

    hjkhl

--- lec_task_5.py
+++ lec_task_5.py
@@ -6,22 +6,21 @@ M = int(input("Введите количество столбцов M: "))
 
 trigonometry_array = np.zeros((N, M))
 
-print(f"В массиве {M} столбцов (номера от 0 до {M-1})")
-a = int(input("Введите номер первого столбца для обмена: "))
-b = int(input("Введите номер второго столбца для обмена: "))
+for i in range(N):      
+    for j in range(M):  
+        value = math.sin(2*N - j)
+        
+        if value < 0:
+            trigonometry_array[i, j] = 0
+        else:
+            trigonometry_array[i, j] = value
 
-if a < 0 or a >= M or b < 0 or b >= M:
-    print("Ошибка! Неправильные номера столбцов")
-else:
-    new_array = trigonometry_array.copy()
-    
-    for i in range(N):
-        temp = new_array[i, a]
-        new_array[i, a] = new_array[i, b]
-        new_array[i, b] = temp
-    
-    print(f"\nМассив после замены столбцов {a} и {b}:")
-    print(new_array)
-    
-    print("\nДля сравнения - исходный массив:")
-    print(trigonometry_array)
+print("Двумерный массив trigonometry_array:")
+print(trigonometry_array)
+
+a = int(input('1 столбец для замены: '))
+b = int(input('2 столбец для замены: '))
+
+trigonometry_array[:, [a, b]] = trigonometry_array[:, [b, a]]
+print()
+print(trigonometry_array)
