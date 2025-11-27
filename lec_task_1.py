545fb3cf4d944996cf227e4e49f62b471e060396
worthless01 <worthless0302@gmail.com>
2025-11-27 15:17:27 +0000

    eeeeeeee

--- lec_task_1.py
+++ lec_task_1.py
@@ -1,14 +1,22 @@
-def a():
+# def a():
     
-    n = int(input('кол-во чисел: '))
-    numbers = []
-    for i in range(n):
-        num = int(input(f'Введите число {i+1}: '))
-        numbers.append(num)
+#     n = int(input('кол-во чисел: '))
+#     numbers = []
+#     for i in range(n):
+#         num = int(input(f'Введите число {i+1}: '))
+#         numbers.append(num)
         
-    arithmetic = sum(numbers) / n
-    return arithmetic
+#     arithmetic = sum(numbers) / n
+#     return arithmetic
 
-result = a()
-print(f'ср арифметическое равно:{result}')
-    
+# result = a()
+# print(f'ср арифметическое равно:{result}')
+
+import numpy as np
+
+def mean_func(array):
+    pass
+
+
+test = np.array([3, 5, 6, 7, 8])
+mean_func(test)
--- lec_task_3.py
+++ lec_task_3.py
@@ -3,7 +3,7 @@ import constant_module as cm
 def mechanical_energy():
     ep = cm.m * cm.g * cm.h
     ek = (cm.m * cm.v ** 2) / 2
-    e = ep / ek
+    e = ep + ek
 
     return e
 
