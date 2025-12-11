0d1cc214382d7f10d1477ef1f005418363441eca
worthless01 <worthless0302@gmail.com>
2025-12-11 14:39:24 +0000

    rgwgharg

--- lec_task_3.py
+++ lec_task_3.py
@@ -17,3 +17,13 @@ end_time = time.time()
 
 time_ = end_time - start_time
 print(f'общее время: {time_} секунд')
+
+
+
+
+for i in range(2):
+    print(f'i: {i}')
+    time.sleep(1)
+    for j in range(3):
+        print(f'\t j: {j}')
+        time.sleep(1)
--- lec_task_4.py
+++
@@ -1,8 +0,0 @@
-flowers = ['роза', 'тюльпан', 'ромашка']
-colors = [' красный', 'желтый', 'белый', 'фиолетовый', 'синий']
-
-flowers_colors = dict(zip(flowers, colors))
-
-print('cловарь: ')
-for i, j in flowers_colors.items():
-    print(f'{i}: {j}')
